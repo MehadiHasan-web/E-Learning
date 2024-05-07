14952137fe4d101ed17cee333dc6bff9ee22df98
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-05-07 16:51:00 +0600

    adding cart

--- src/app/components/courseCart.jsx
+++ src/app/components/courseCart.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import Image from 'next/image';
+import learningImg from '../../../public/courseCartImg/learning.png'
 
 const CourseCart = () => {
   return (
     <div>
-      
+      {/* cart content section start */}
+      <div>
+        {/* image section start */}
+        <div className='h-50% w-full overflow-hidden'>
+          <Image src={learningImg} alt="learning image"></Image>
+        </div>
+        {/* image section end */}
+      </div>
+      {/* cart content section end */}
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,27 @@
 import Image from "next/image";
 import Button from '@mui/material/Button';
+import CourseCart from "./components/courseCart";
 
 export default function Home() {
   return (
     <main>
       {/* find our course section start */}
-      <div className="container"></div>
+      <div className="container mx-auto xl:py-10 xl:px-20">
+        {/* title section start */}
+        <div className="text-center">
+          <h1 className="2xl:text-5xl font-bold">Find Your <span className="text-orange-600">Course</span></h1>
+        </div>
+        {/* title section end */}
+        {/* courseCart section start */}
+        <div className="grid grid-cols-3 gap-10">
+        <CourseCart></CourseCart>
+        <CourseCart></CourseCart>
+        <CourseCart></CourseCart>
+        <CourseCart></CourseCart>
+        </div>
+        
+        {/* courseCart section end */}
+      </div>
       {/* find our course section end */}
     </main>
   );
